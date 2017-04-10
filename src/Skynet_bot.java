f87b2e4080e422c251e879e544934504cf7326a8
Mikhail <mhtr.ko@gmail.com>
2017-04-10 19:09:51 +0700

    restructured of bot, creating new class

--- src/Skynet_bot.java
+++ src/Skynet_bot.java
@@ -1,17 +1,5 @@
 import java.awt.AWTException;
-import java.awt.Image;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.awt.image.*;
-import java.awt.Toolkit;
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.win32.StdCallLibrary;
@@ -29,7 +17,7 @@ public class Skynet_bot {
          boolean SetWindowText(HWND hwnd, String newText);
     }
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, AWTException {
 		
 		HWND hwnd = User32.instance.FindWindow("gens", null);
         if(hwnd == null) {
@@ -42,33 +30,8 @@ public class Skynet_bot {
         System.out.println("Нашли окно: "+hwnd);
         User32.instance.SetForegroundWindow(hwnd);
         
-        try {
-            Robot robot = new Robot();
-
-            // Robot start writting
-            for(int i = 0; i < 5; i++){
-             
-            	/*robot.delay(2000);
-            	robot.keyPress(KeyEvent.VK_D);
-            	robot.delay(3000);
-            	
-            	robot.keyPress(KeyEvent.VK_K);
-            	robot.delay(100);
-            	robot.keyRelease(KeyEvent.VK_K);
-            	*/
-            }
-            robot.keyPress(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_PRINTSCREEN);
-            robot.delay(100);
-            robot.keyRelease(KeyEvent.VK_ALT);
-            robot.keyRelease(KeyEvent.VK_PRINTSCREEN);
-
-        } catch (AWTException e) {
-
-            e.printStackTrace();
-
-        }
-        
+        Bot bot = new Bot();
+        bot.prtSc();
         
         Window w1 = new  Window();
     	w1.loadImage();

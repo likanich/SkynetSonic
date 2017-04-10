b00ca9e27ba120c5ea2396188f2e86a1f48ba3fd
Vasiliy <likanich152@gmail.com>
2017-04-10 20:09:52 +0700

    synchronize

--- src/Skynet_bot.java
+++ src/Skynet_bot.java
@@ -1,41 +1,18 @@
 import java.awt.AWTException;
 import java.io.IOException;
-import com.sun.jna.Native;
-import com.sun.jna.platform.win32.WinDef.HWND;
-import com.sun.jna.win32.StdCallLibrary;
-import com.sun.jna.win32.W32APIOptions;
 
 public class Skynet_bot {
 	
-	public static interface User32 extends StdCallLibrary
-    {
-         final User32 instance = (User32) Native.loadLibrary ("user32", User32.class, 
-                          W32APIOptions.UNICODE_OPTIONS);
-         HWND FindWindow(String className, String windowName);
-         HWND SetFocus(HWND hWnd);
-         boolean SetForegroundWindow(HWND hWnd);
-         boolean SetWindowText(HWND hwnd, String newText);
-    }
-
 	public static void main(String[] args) throws IOException, AWTException {
 		
-		HWND hwnd = User32.instance.FindWindow("gens", null);
-        if(hwnd == null) {
-            hwnd = User32.instance.FindWindow(null, "gens");
-        }
-        if(hwnd == null) {
-            System.out.println("Окно не найдено");
-            return;
-        }
-        System.out.println("Нашли окно: "+hwnd);
-        User32.instance.SetForegroundWindow(hwnd);
-        
+		Window window = new  Window();
+        window.interception();
+		
         Bot bot = new Bot();
         bot.prtSc();
         
-        Window w1 = new  Window();
-    	w1.loadImage();
-    	w1.drawIcon();
+    	window.loadImage();
+    	window.drawIcon();
         
 	}
 
--- src/Window.java
+++ src/Window.java
@@ -1,73 +1,89 @@
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import java.awt.Image;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.win32.StdCallLibrary;
+import com.sun.jna.win32.W32APIOptions;
 import java.awt.Toolkit;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.image.RenderedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class Window extends JFrame {
 
-private int width = 300;
-private int height = 300;
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int width = 300;
+	private int height = 300;
 
-private BufferedImage img = null;
+	private BufferedImage img = null;
 
-public Window() {
-    setSize(width, height);
-    setTitle("File Monitor:D");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLocationRelativeTo(null);
-    setResizable(false);
-    setVisible(true);
+	public static interface User32 extends StdCallLibrary //interface for win32 
+	{
+	     final User32 instance = (User32) Native.loadLibrary ("user32", User32.class, 
+	                      W32APIOptions.UNICODE_OPTIONS);
+	     HWND FindWindow(String className, String windowName);
+	     HWND SetFocus(HWND hWnd);
+	     boolean SetForegroundWindow(HWND hWnd);
+	     boolean SetWindowText(HWND hwnd, String newText);
+	}
 
-}
-
-public void loadImage() throws IOException {
-    //img = new ImageIcon(src).getImage();
-    //img = ImageIO.read(new File(src));
-	img = getImageFromClipboard();
-}
-
-public void drawIcon() {
-    Graphics g = getGraphics();
-    g.drawImage(img, 0, 0, width, height, null);
-}
-public static BufferedImage getImageFromClipboard(){
-	  Transferable transferable = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
-	  if (transferable != null && transferable.isDataFlavorSupported(DataFlavor.imageFlavor))
-	  {
-	    try
-	    {
-	      return (BufferedImage) transferable.getTransferData(DataFlavor.imageFlavor);
+	public Window() {
+	    setSize(width, height);
+	    setTitle("File Monitor");
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setLocationRelativeTo(null);
+	    setResizable(false);
+	    setVisible(true);
+	
+	}
+	
+	public void interception () { //inerception window with Sonic
+		HWND hwnd = User32.instance.FindWindow("gens", null);
+	    if(hwnd == null) {
+	        hwnd = User32.instance.FindWindow(null, "gens");
 	    }
-	    catch (UnsupportedFlavorException e)
-	    {
-	      // handle this as desired
-	      e.printStackTrace();
+	    if(hwnd == null) {
+	        System.out.println("Окно не найдено");
+	        return;
 	    }
-	    catch (IOException e)
-	    {
-	      // handle this as desired
-	      e.printStackTrace();
-	    }
-	  }
-	  else
-	  {
-	    System.err.println("getImageFromClipboard: That wasn't an image!");
-	  }
-	  return null;
+	    System.out.println("Нашли окно: "+hwnd);
+	    User32.instance.SetForegroundWindow(hwnd);
+	}
+	
+	public void loadImage() throws IOException {// load image from clipboard
+	
+		img = getImageFromClipboard();
+	}
+	
+	public void drawIcon() { // show image on new window
+	    Graphics g = getGraphics();
+	    g.drawImage(img, 0, 0, width, height, null);
+	}
+	
+	public static BufferedImage getImageFromClipboard(){ //get image from clipboard
+		Transferable transferable = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+		if (transferable != null && transferable.isDataFlavorSupported(DataFlavor.imageFlavor)){
+		    try{
+		    	return (BufferedImage) transferable.getTransferData(DataFlavor.imageFlavor);
+		    }
+		    catch (UnsupportedFlavorException e){
+		      // handle this as desired
+		      e.printStackTrace();
+		    }
+		    catch (IOException e){
+		      // handle this as desired
+		      e.printStackTrace();
+		    }
+		}
+		else{
+		    System.err.println("getImageFromClipboard: That wasn't an image!");
+		}
+		return null;
 	}
 
 }
